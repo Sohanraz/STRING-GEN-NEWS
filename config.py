921bf086a67761c0760c0432a479e13a2c256528
Sohan rajpurohit <132225307+Sohanraz@users.noreply.github.com>
2024-06-13 20:45:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_ID = int(getenv("API_ID","29219170"))
 API_HASH = getenv("API_HASH", "12d6648ede66e1ef31d9c455317ee09d")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "6282594199:AAFXICV7X6uAOH2nELP3IruuU5o5TFylcBM")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Sohanrazz:<password>@cluster0.wbcmn82.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Sohanrazz:Sohanrazz@cluster0.wbcmn82.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 OWNER_ID = int(getenv("OWNER_ID", "5897793065"))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/purohit_Bots_Support")
